727e10581d9e47981db72850e1770fa150c34365
Tarnasa <tarnasa7@gmail.com>
2016-12-08 21:43:46 -0600

    Join endpoints of skeleton paths

--- contours.py
+++ contours.py
@@ -13,6 +13,10 @@ from scipy import ndimage
 from scipy.spatial.distance import pdist
 from scipy.spatial import KDTree
 
+from itertools import combinations
+import math
+from collections import namedtuple
+
 
 def skel_to_graph(skel):
     """
@@ -63,6 +67,7 @@ def skel_to_vectors(skel):
     xn, yn = node_indices = np.nonzero(nodes)
     used_edges = set([0])
     paths = {}  # edge_id -> path
+    minipaths = []
     for n in range(len(xn)):
         while True:
             x, y = xn[n], yn[n]
@@ -105,17 +110,83 @@ def skel_to_vectors(skel):
                 # Remove the edge label so that it isn't chosen as the next pixel's neighbor
                 edges[px, py] = 0
             paths[edge_id] = np.array(path)
-    return paths.values()
+    return paths.values() + minipaths
+    # TODO: Handle loops with no nodes
+
+
+def join_segments(paths, threshold):
+    """
+    segments is like:
+    [path1, path2]
+    where path1 is like
+    [[x1, y1], [x2, y2], [x3, y3]]
+
+    This function looks at the endpoints of paths, and if they are
+    close enough, moves them to their midpoint.
+
+    This function modifies paths in-place
+    """
+    Group = namedtuple('Group', ('indices', 'points'))
+    def midpoint(points):
+        xa = round(sum(x for x, y in points) / float(len(points)))
+        ya = round(sum(y for x, y in points) / float(len(points)))
+        return xa, ya
+
+    groups = []  # [Group]
+    endpoints = []  # [(path_index, index)]
+    for pi1, p1 in enumerate(paths):
+        indices = [0] + ([len(p1)-1] if len(p1) else [])
+        for ei1 in indices:
+            e1 = p1[ei1]
+            endpoints.append((pi1, ei1))
+            x, y = paths[pi1][ei1]
+            group = Group([[pi1, ei1]], [[x, y]])
+            groups.append(group)
+
+    for pi, ei in endpoints:
+        x, y = paths[pi][ei]
+        found = False
+        source_group_index = None
+        for gi, group in enumerate(groups):
+            if (x, y) in group.points:
+                source_group_index = gi
+                if len(group) != 1:
+                    found = False
+                    break
+                continue
+            mx, my = midpoint(group.points + [(x, y)])
+            if math.hypot(mx-x, my-y) < threshold:
+                group.points.append((x, y))
+                group.indices.append((pi, ei))
+                found = True
+        if found and source_group_index:
+            del group[source_group_index]
+
+    for group in groups:
+        if len(group.points) > 1:
+            print(group.points)
+            mx, my = midpoint(group.points)
+            for pi, ei in group.indices:
+                paths[pi][ei] = mx, my
+
+    # TODO
+
+
+def to_line_segments(paths):
+    """
+    """
+    pass
 
 
 # Open image and threshold
-image = io.imread('out/125.png')  # 36 for $, 79 for O
+image = io.imread('test.png')  # 36 for $, 79 for O
 im = np.min(image, axis=2) < 20
 
 # Skeletonize to get 1-pixel-thick lines
 skel = morphology.skeletonize(im)
 nodes, edges, length_edges = skel_to_graph(skel)
 paths = skel_to_vectors(skel)
+join_segments(paths, 2)
 #print(repr(paths))
 
 # blurred = 3**skel.ndim * ndimage.uniform_filter(skel.astype(np.float))
@@ -127,10 +198,10 @@ paths = skel_to_vectors(skel)
 # Visualize results
 #plt.figure(figsize=(10, 10))
 fig, ax = plt.subplots()
-ax.imshow(im, cmap='gray', interpolation='nearest')
+ax.imshow(skel, cmap='gray', interpolation='nearest')
 #paths[1] = measure.approximate_polygon(paths[1], 2)
 for path in paths:
-    path = measure.approximate_polygon(path, 1)
+    path = measure.approximate_polygon(path, 0.7)  # Always < 1, or else it can kill endpoints
     ax.plot(path[:, 1], path[:, 0], linewidth=2)
 
     ax.axis('image')
